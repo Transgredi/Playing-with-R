3638a717bd979f7b47b2299e49884263528a3550
Tymon <transgredi@gmail.com>
2015-06-22 22:44:50 +0200

    no message

---
+++ pogromcydanych/czyszczenie_danych.R
@@ -0,0 +1,6 @@
+“DF <- data.frame(
+  imie = c("Maja","Anna","Zosia","Anna"),
+  wiek = c("40","12,5","25","16.6"),
+  numer = c(1,2,NA,4),
+  oczo = factor(c("niebieskie", "jasno-niebieskie", "ciemne", "niebieskie")),
+  stringsAsFactors = FALSE)”
