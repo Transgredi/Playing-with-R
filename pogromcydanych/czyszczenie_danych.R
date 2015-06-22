6c82d32af45da945e882879734771f53edadd985
Tymon <transgredi@gmail.com>
2015-06-22 22:56:08 +0200

    no message

--- pogromcydanych/czyszczenie_danych.R
+++ pogromcydanych/czyszczenie_danych.R
@@ -1,6 +1,14 @@
-“DF <- data.frame(
+DF <- data.frame(
   imie = c("Maja","Anna","Zosia","Anna"),
   wiek = c("40","12,5","25","16.6"),
   numer = c(1,2,NA,4),
   oczo = factor(c("niebieskie", "jasno-niebieskie", "ciemne", "niebieskie")),
-  stringsAsFactors = FALSE)”
+  stringsAsFactors = FALSE)
+
+colnames(DF)[4] <- "oczy"
+
+# Dzieki nawiasom wynik przypisania zostanie
+# wyswietlony na ekranie.
+(tmp <- gsub(pattern = ",", replacement = ".", DF$wiek))
+
+DF$wiek <- as.numeric(tmp)
